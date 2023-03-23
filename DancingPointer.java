f967abafa67bbf1a6830872d1f24ab9d288fdf81
Gaurav Nyaupane <mail.gauravnyaupane@gmail.com>
2023-03-23 12:36:20 +0545

    Updated dancingpointerwithclick

--- DancingPointer.java
+++ DancingPointer.java
@@ -15,8 +15,9 @@ public class DancingPointer {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws AWTException {
-        while(true){
-            new Robot().mouseMove((int) (Math.random() * (Toolkit.getDefaultToolkit().getScreenSize().width+1)), (int) (Math.random() * (Toolkit.getDefaultToolkit().getScreenSize().height+1)));
-        }
+        while(true) new Robot().mouseMove((int) (Math.random() * (Toolkit.getDefaultToolkit().getScreenSize().width+1)), (int) (Math.random() * (Toolkit.getDefaultToolkit().getScreenSize().height+1)));
     }
 }
+
+// convert to jar using this command
+// javac -cp . DancingPointer.java && jar cvfe DancingPointer.jar DancingPointer DancingPointer.class
